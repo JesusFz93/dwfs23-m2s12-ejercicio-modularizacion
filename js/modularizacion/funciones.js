cb00dc09cf62a8c50f6205f91d21c91105b27527
Jesus <jesus.fdz93@gmail.com>
2023-12-02 11:32:10 -0600

    agregue tal cosa

---
+++ js/modularizacion/funciones.js
@@ -0,0 +1,12 @@
+import { promesa1 } from "./promesas.js";
+
+const llamarUsuarios = async () => {
+  try {
+    const respuesta = await promesa1;
+    console.log(respuesta);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { llamarUsuarios };
